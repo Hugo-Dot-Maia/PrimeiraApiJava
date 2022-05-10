615ff26fe9722f95f6e1772ae5899629ace48289
hugog <hugoguima75@outlook.com>
2022-05-10 11:16:08 -0300

    Added test

--- src/test/java/hugoAPI/demo/PrimeiraRestApiApplicationTests.java
+++ src/test/java/hugoAPI/demo/PrimeiraRestApiApplicationTests.java
@@ -3,11 +3,14 @@ package hugoAPI.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class PrimeiraRestApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(4,2+2);
 	}
 
 }
